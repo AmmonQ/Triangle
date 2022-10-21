c92f1e9964a521adf63e6141e22cfe595281c654
Ammon Quackenbush <aq229@byu.edu>
2022-10-21 10:09:22 -0600

    Cleaned up some code

--- view.py
+++ view.py
@@ -1,8 +1,4 @@
-# This is a sample Python script.
 import sys
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit
 
